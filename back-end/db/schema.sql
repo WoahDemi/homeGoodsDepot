53378f62db7652bee21f664ad01bdea9cf67f0ec
Demi <oluwademiladejobi@pursuit.org>
2021-07-20 14:40:50 -0400

    added seed data and configured schema

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS test;
 
-CREATE TABLE test (
+CREATE TABLE homes (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    address TEXT,
+    state VARCHAR(2),
+    number_of_bathrooms INTEGER,
+    number_of_bedrooms INTEGER,
+    price INTEGER,
+    parking BOOLEAN,
+    property_type TEXT,
+    saved BOOLEAN,
+
 );
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,8 +1,19 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO homes (address, state, number_of_bathrooms,number_of_bedrooms, price, parking, property_type, saved) VALUES
+('353 Juniper Drive, Oyster Bay', 'NY', 9, 6, 8495000, TRUE, 'SFH', FALSE),
+('3335 21st Street, San Francisco', 'CA', 3, 3, 1295000, FALSE, 'Condo', FALSE),
+('900 Brickell Key Boulevard, Miami', 'FL', 6, 4, 25000000, TRUE, 'Condo', FALSE),
+('36404 Highway 1, Monterey', 'CA', 6, 5, 24500000, TRUE, 'SFH', FALSE),
+('3840 E Placita Sumo, Tucson', 'AZ', 5, 4, 1449950, TRUE, 'SFH', FALSE),
+('2909 Cleburne St, Houston', 'TX', 2, 4, 495000, TRUE, 'MFH', FALSE),
+('1120 Cedar St, Jacksonville', 'FL', 7, 8, 1249000, TRUE, 'MFH', FALSE),
+('17353 Beaver St W, Jacksonville', 'FL', 8, 12, TRUE, 1850000, 'MFH', FALSE),
+('25211 Jim Bridger Road, Hidden Hills', 'CA', 9, 7, 13995000, TRUE, 'SFH', FALSE),
+('25354 Prado De La Felicidad, Calabasas', 'CA', 14, 8, 34000000, TRUE, 'SFH', FALSE),
+('1428 S Concord St W, Salt Lake City', 'UT', 3, 3, 430000, TRUE, 'SFH', FALSE),
+('1100 Arthur St, Iowa City', 'IA', 4, 6, 3500000, TRUE, 'MFH', FALSE),
+('300 S Clinton St APT706, Iowa City', 'IA', 3, 2, 1035300, TRUE, 'Condo', FALSE)
+
+
+
+
+// SFH Condo MFH 
