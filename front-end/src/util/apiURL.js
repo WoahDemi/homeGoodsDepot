d80fd4b74a8e8ed1087d0aeb46fa79fb7de9214d
Demi <oluwademiladejobi@pursuit.org>
2021-07-19 21:09:48 -0400

    API edit

--- front-end/src/util/apiURL.js
+++ front-end/src/util/apiURL.js
@@ -1,5 +1,5 @@
 export const apiURL = () => {
   return window.location.hostname === "localhost"
     ? "http://localhost:3333"
-    : "https://lit-fortress-73842.herokuapp.com/";
+    : "https://lit-fortress-73842.herokuapp.com";
 };
