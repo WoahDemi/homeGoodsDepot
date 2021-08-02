19d5459822e6bc3538ea90cf63b38e483ba6e95a
Shoaib Dar <SDAR30@gmail.com>
2021-08-01 20:36:19 -0400

    landingpage styling

--- front-end/src/Pages/LandingPage.js
+++ front-end/src/Pages/LandingPage.js
@@ -1,27 +1,39 @@
 import "../Styles/LandingPage.css"
-import {Container, Row, Col, Image, Button} from "react-bootstrap"
-import {Link} from "react-router-dom"
+import { Container, Card, Modal, Image, Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 function Home() {
 
-  
-    return (
-      <div className="Home" >
-        <Container>
-          <Row className="align-items-center" >
-            <Col  >
-              <h2>homeGoodsDepot</h2>
-              <h3>We turn rich dreams to reality.</h3>
-              <Link to={`/homes`}>
-                <Button variant="outline-primary">Find a Home</Button>
-              </Link>
-            </Col>
-            <Col >
-              <Image className="image" src="https://www.priceypads.com/wp-content/uploads/2018/05/ISa1ba7g0qkjdn0000000000-2.jpg"  />  
-            </Col>
-          </Row>
-        </Container>
+
+  return (
+    <div className="Home" >
+
+
+      {/* <div className="div-card">
+      <Card className="text-center bg-transparent w-25" >
+        <Card.Body>
+          <Card.Title>HomeGoods Depot</Card.Title>
+          <Card.Text>
+            We turn rich dreams to reality.
+          </Card.Text>
+          <Link to={`/homes`}>
+            <Button variant="primary">Find a Home</Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      </div> */}
+
+
+
+      <div className="find-home-div">
+        <Link to={`/homes`}>
+          <Button className="find-home-button" variant="dark btn-lg btn-block" >Find a Home Today</Button>
+        </Link>
       </div>
-    );
-  }
-  
-  export default Home;
+
+      <Image className="image" src="https://i.ytimg.com/vi/mXPjoWckRPg/maxresdefault.jpg" />
+
+    </div>
+  );
+}
+
+export default Home;
