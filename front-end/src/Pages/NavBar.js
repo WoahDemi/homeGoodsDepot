52b38d7539c83dabe0ba107bf618989af03af643
Shoaib Dar <SDAR30@gmail.com>
2021-08-04 21:40:12 -0400

    responsive navbar with active class

--- front-end/src/Pages/NavBar.js
+++ front-end/src/Pages/NavBar.js
@@ -1,24 +1,25 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "../Styles/NavBar.css"
 
 export default function NavBar() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container className="nav-format">
         <Nav>
-          <Navbar.Brand className="nav-text" href="/">
-            🏡 HomeGoods Depot 🏡
-          </Navbar.Brand>
+            <NavLink className="nav-text" exact to="/">
+            🏡 HomeGoods Depot 🏡 
+            </NavLink>
         </Nav>
         <Nav>
-          <Nav.Link className="nav-text" href="/homes">
+          <NavLink className="nav-text"  exact to="/homes">
             Available HomeGoods
-          </Nav.Link>
+            </NavLink>
         </Nav>
         <Nav>
-          <Nav.Link className="nav-text" href="/homes/new">
+        <NavLink className="nav-text"  exact to="/homes/new">
             Add a Listing
-          </Nav.Link>
+            </NavLink>
         </Nav>
       </Container>
     </Navbar>
