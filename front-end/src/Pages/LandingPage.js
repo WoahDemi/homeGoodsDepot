5563f985b4d4ef9a2c6fe2b21b7a1e648ddd0ea1
Shoaib Dar <SDAR30@gmail.com>
2021-08-07 10:38:45 -0400

    responsive landing page style

--- front-end/src/Pages/LandingPage.js
+++ front-end/src/Pages/LandingPage.js
@@ -1,5 +1,5 @@
 import "../Styles/LandingPage.css"
-import { Container, Card, Modal, Image, Button } from "react-bootstrap"
+import { Modal, Image, Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 function Home() {
 
@@ -7,30 +7,18 @@ function Home() {
   return (
     <div className="Home" >
 
-
-      {/* <div className="div-card">
-      <Card className="text-center bg-transparent w-25" >
-        <Card.Body>
-          <Card.Title>HomeGoods Depot</Card.Title>
-          <Card.Text>
-            We turn rich dreams to reality.
-          </Card.Text>
-          <Link to={`/homes`}>
-            <Button variant="primary">Find a Home</Button>
-          </Link>
-        </Card.Body>
-      </Card>
-      </div> */}
-
-
-
       <div className="find-home-div">
-        <Link to={`/homes`}>
-          <Button className="find-home-button" variant="dark btn-lg btn-block" >Find a Home Today</Button>
-        </Link>
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title ><p className="modal-title">HomeGoods Depot</p></Modal.Title>
+            </Modal.Header>
+            <Link to={`/homes`}>
+              <Button className="find-home-button" variant="dark btn-lg btn-block m-2" >Find a Home Today</Button>
+            </Link>
+        </Modal.Dialog>
       </div>
 
-      <Image className="image" src="https://3sdwfb1xa06f3y7ie9255ali-wpengine.netdna-ssl.com/na/wp-content/uploads/sites/8/house-with-grey-shingles-backyard-pool.jpg"/>
+      <Image className="image" src="https://3sdwfb1xa06f3y7ie9255ali-wpengine.netdna-ssl.com/na/wp-content/uploads/sites/8/house-with-grey-shingles-backyard-pool.jpg" />
 
     </div>
   );
